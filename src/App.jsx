0ec2641c2204b2e1120bf3ec199d404185668fd2
gurjarkunal <kunalgurjarkg786@gmail.com>
2024-08-14 00:25:48 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import PlaceOrder from "./pages/Placeorder/PlaceOrder";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
+
+function App() {
+  const [showLogin, setShowLogin] = useState(false);
+  return (
+    <>
+      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
+      <div className="app">
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/order" element={<PlaceOrder />}></Route>
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Cart/Cart.jsx
@@ -0,0 +1,72 @@
+import React, { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
+import { useNavigate } from "react-router-dom";
+function Cart() {
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
+  const navigate = useNavigate();
+  return (
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-item-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div className="">
+                <div className="cart-item-title cart-items-item" key={index}>
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p><span className="rupee">₹</span>{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p><span className="rupee">₹</span>{item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">x</p>
+                </div>
+                <hr />
+              </div>
+            );
+          }
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div className="cart-total-details">
+            <p>Subtotal</p>
+            <p><span className="rupee">₹</span>{getTotalCartAmount()}</p>
+          </div>
+            <hr />
+          <div className="cart-total-details">
+            <p>Delivery Fee</p>
+            <p><span className="rupee">₹</span>{getTotalCartAmount() === 0 ? 0 : 2}</p>
+          </div>
+            <hr />
+          <div className="cart-total-details">
+            <b>Total</b>
+            <b><span className="rupee">₹</span>{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
+          </div>
+        <button onClick={() => navigate("/order")}>PROCEED TO CHECKOUT</button>
+        </div>
+      <div className="cart-promocode">
+        <div>
+          <p>if you have a promo code, Enter it here</p>
+          <div className="cart-promocode-input">
+            <input type="text" placeholder="Enter promo code" />
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cart;
